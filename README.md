edb0e955f4a6d9dd06d0e0de1b4b3b21cb233494
Ismail Marzuqi <marzhochi@gmail.com>
2023-09-04 18:32:16 +0700

    upload master

---
+++ README.md
@@ -0,0 +1,21 @@
+# BPJS Kesehatan Wrapper
+PHP package to access BPJS Kesehatan API.
+This package is a simple wrapper of BPJS VClaim Web Service 2.0
+https://dvlp.bpjs-kesehatan.go.id:8888/trust-mark/portal.html
+
+#### Installation :fire:
+
+`composer require marzhochi/bpjs`
+
+
+
+#### Supported Services:
+
+- [x] LPK (Lembar Pengajuan Klaim)
+- [x] Monitoring
+- [x] Peserta
+- [x] PRB (Pembuatan Rujukan Balik)
+- [x] Referensi
+- [x] Rencana Kontrol
+- [x] Rujukan
+- [x] SEP
---
+++ src/Vclaim/Peserta.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Marzhochi\Bpjs\Vclaim;
+
+use Marzhochi\Bpjs\BpjsService;
+
+class Peserta extends BpjsService
+{
+    private static $_instance;
+
+    public function __construct($configuration)
+    {
+        parent::__construct($configuration);
+    }
+
+    public static function getInstance($configuration)
+    {
+        if (isset(self::$_instance))
+            return self::$_instance;
+
+        self::$_instance = new Peserta($configuration);
+
+        return self::$_instance;
+    }
+
+    public function getByNoKartu($noKartu, $tglPelayananSep)
+    {
+        $response = $this->get("Peserta/nokartu/$noKartu/tglSEP/$tglPelayananSep");
+
+        return $response;
+    }
+
+    public function getByNik($nik, $tglPelayananSep)
+    {
+        $response = $this->get("Peserta/nik/$nik/tglSEP/$tglPelayananSep");
+
+        return $response;
+    }
+}
